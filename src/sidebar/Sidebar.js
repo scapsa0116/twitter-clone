c5fbec52ef86c767fedd2a080283413908aaf53f
stela <stela.capsa@gmail.com>
2021-07-06 15:57:22 -0500

    more icons

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -4,6 +4,12 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import SidebarOptions from '../sidebar/SidebarOptions'
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
+import TurnedInNotIcon from '@material-ui/icons/TurnedInNot';
+import EmailIcon from '@material-ui/icons/Email';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';export class Sidebar extends Component {
     render() {
         return (
@@ -12,10 +18,11 @@ import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';export 
                 <SidebarOptions Icon={HomeIcon} text="Home"/>
                 <SidebarOptions Icon={SearchIcon} text="Search"/>
                 <SidebarOptions Icon={NotificationsNoneIcon} text="Notifications"/>
-
-                {/* <SidebarOptions Icon={} text="Home"/>
-                <SidebarOptions Icon={} text="Home"/>
-                <SidebarOptions Icon={} text="Home"/> */}
+                <SidebarOptions Icon={EmailIcon} text="Messages"/>
+                <SidebarOptions Icon={TurnedInNotIcon} text="Bookmarks"/>
+                <SidebarOptions Icon={ListAltIcon} text="Lists"/>
+                <SidebarOptions Icon={PersonOutlineIcon} text="Profile"/>
+                <SidebarOptions Icon={MoreHorizIcon} text="More"/>
             </div>
         )
     }
