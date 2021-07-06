3e477c4ffb487a0b7e15bfc9e3b34c57e48547e9
stela <stela.capsa@gmail.com>
2021-07-06 14:55:57 -0500

    app progect ui icons installed

---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Sidebar from './sidebar/Sidebar.css'
+
+export class Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Sidebar
